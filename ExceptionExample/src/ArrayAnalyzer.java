097c07b344ce2c0bb109ace90a1c11545baf970d
Marazair <nick1022m@gmail.com>
2017-03-31 16:42:04 -0400

    Created an array, then created a function to break it.

--- ExceptionExample/src/ArrayAnalyzer.java
+++ ExceptionExample/src/ArrayAnalyzer.java
@@ -1,9 +1,22 @@
 
 public class ArrayAnalyzer {
-
+	private static final int ARRAY_SIZE = 10;
+	private static final int MEANINGLESS_DATA = 0;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int array[] = new int[ARRAY_SIZE];
+		CreateArray(array);
 	}
-
+	
+	public static void CreateArray(int[] array){
+		for(int x = 0; x < ARRAY_SIZE; x++){
+			array[x] = MEANINGLESS_DATA;
+		}
+		
+		BreakArray(array);
+	}
+	
+	public static void BreakArray(int[] array){
+		System.out.println(array[ARRAY_SIZE]);
+	}
 }
