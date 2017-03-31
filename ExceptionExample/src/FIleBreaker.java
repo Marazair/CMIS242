f417219fe5d1faf8f372dfd9cdcd39879a614246
Marazair <nick1022m@gmail.com>
2017-03-31 17:36:29 -0400

    Finished
    Finished the FileBreaker

--- ExceptionExample/src/FIleBreaker.java
+++ ExceptionExample/src/FIleBreaker.java
@@ -1,9 +1,28 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class FIleBreaker {
 	private static final String NONEXISTANT_FILENAME = "dhfldasjflkdsajl";
 	
-	public static void main(String args[]){
-		
+	public static void main(String args[]) {
+		ReadFile();
 	}
+	
+	public static void ReadFile() {
+		try {
+			Scanner reader = CreateFileReader();
+			while(reader.hasNext()){
+				System.out.print(reader.next());
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Invalid File Name.");
+		}
+	}
+	
+	public static Scanner CreateFileReader() throws FileNotFoundException {
+		Scanner s = new Scanner(new File(NONEXISTANT_FILENAME));
+		return s;
+	}
+	
 }
