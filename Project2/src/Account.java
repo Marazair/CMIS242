4a6736f0e47c74e566d015e7ab412e0a68fb22b6
Marazair <nick1022m@gmail.com>
2017-04-05 15:55:06 -0400

    Account Changes
    Created withdraw, deposit, transfer and a constructor for Account.

---
+++ Project2/src/Account.java
@@ -0,0 +1,40 @@
+/*
+ * File Name: Account.java
+ * Name: Nick Mills
+ * Date: 4/7/17
+ * Purpose: 
+ */
+public class Account {
+	private static final double SERVICE_CHARGE = 1.50;
+	
+	private static int withdrawls = 0;
+	private double accountTotal = 0;
+	
+	public Account(double accountTotal) {
+		this.accountTotal = accountTotal;
+	}
+	
+	public void withdraw(double amount) throws InsufficientFunds {
+		if(withdrawls >= 4){
+			amount += SERVICE_CHARGE;
+		}
+		
+		if(accountTotal - amount >= 0){
+			accountTotal -= amount;
+			withdrawls++;
+		}
+		else{
+			InsufficientFunds i = new InsufficientFunds();
+			throw i;
+		}
+	}
+	
+	public void deposit(double amount){
+		accountTotal += amount;
+	}
+	
+	public void transfer(double amount, Account otherAccount) throws InsufficientFunds {
+		withdraw(amount);
+		otherAccount.deposit(amount);
+	}
+}
