266994021ae4f3b00c3b0445b89b20702c23c6e0
Marazair <nick1022m@gmail.com>
2017-04-06 13:46:41 -0400

    Accounts and amountField
    Created account objects and began to add code to create a text field for
    amount input.

--- Project2/src/BankGUI.java
+++ Project2/src/BankGUI.java
@@ -8,10 +8,14 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.text.*;
 
 public class BankGUI extends JPanel implements ActionListener{
 	
 	public BankGUI(){
+		Account checkingAccount = new Account(0);
+		Account savingsAccount = new Account(0);
+		
 		JButton withdraw = new JButton("Withdraw");
 		withdraw.setActionCommand("withdraw");
 		
@@ -24,10 +28,24 @@ public class BankGUI extends JPanel implements ActionListener{
 		JButton balance = new JButton("Balance");
 		balance.setActionCommand("balance");
 		
+		JRadioButtonMenuItem checking = new JRadioButtonMenuItem("Checking");
+		checking.setActionCommand("checking");
+		
+		JRadioButtonMenuItem savings = new JRadioButtonMenuItem("Savings");
+		checking.setActionCommand("savings");
+		
+		ButtonGroup accounts = new ButtonGroup();
+		accounts.add(savings);
+		accounts.add(checking);
+		
+		JFormattedTextField amountField = new JFormattedTextField(NumberFormat.getCurrencyInstance());
+		
 		withdraw.addActionListener(this);
 		deposit.addActionListener(this);
 		transfer.addActionListener(this);
 		balance.addActionListener(this);
+		savings.addActionListener(this);
+		checking.addActionListener(this);
 		
 		withdraw.setToolTipText("Withdraw the typed amount from the selected account. (Must be in increments of $20)");
 		deposit.setToolTipText("Deposit the typed amount to the selected account.");
@@ -38,7 +56,11 @@ public class BankGUI extends JPanel implements ActionListener{
 		add(deposit);
 		add(transfer);
 		add(balance);
+		add(checking);
+		add(savings);
+		add(amountField);
 	}
+	
 	public static void main(String args[]) {
 		JFrame frame = new JFrame("ATM");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,7 +74,18 @@ public class BankGUI extends JPanel implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-			 
+		if("withdraw".equals(e.getActionCommand())) {
+			
+		}
+		else if("deposit".equals(e.getActionCommand())) {
+			
+		}
+		else if("transfer".equals(e.getActionCommand())){
+			
+		}
+		else if("balance".equals(e.getActionCommand())){
+			
+		}
 		
 	}
 }
