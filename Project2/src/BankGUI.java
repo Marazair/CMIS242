8cf9aadec962fa6a30f579bd48331ea3734f54a1
Marazair <nick1022m@gmail.com>
2017-04-08 19:25:56 -0400

    Comments
    Commented up the code in BankGUI.

--- Project2/src/BankGUI.java
+++ Project2/src/BankGUI.java
@@ -13,6 +13,8 @@ import java.text.*;
 import java.beans.*;
 
 public class BankGUI extends JPanel implements ActionListener, PropertyChangeListener {
+	
+	//These are the values which will be passed to the Account constructors for the savings and checking accounts.
 	private static final double CHECKING_DEFAULT_AMOUNT = 0;
 	private static final double SAVINGS_DEFAULT_AMOUNT = 0;
 	
@@ -22,21 +24,29 @@ public class BankGUI extends JPanel implements ActionListener, PropertyChangeLis
 	private Account savingsAccount;
 	private double amount = 0;
 	private AmountField amountField;
+	
+	//Create a frame for the JOptionPanes.
 	private static JFrame PopupFrame = new JFrame("PopUp");
+	
 	private NumberFormat currency = NumberFormat.getCurrencyInstance();
 	
 	public BankGUI() {
+		
+		//Create the main panel, padding the edges.
 		super.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
 		this.setBorder(padding);
 		
+		//Create panels for storing the different types of buttons and fields and lay them out appropriately.
 		JPanel transactionButtons = new JPanel(new GridLayout(2,2,10,10));
 		JPanel accountButtons = new JPanel(new FlowLayout());
 		JPanel textPanel = new JPanel(new FlowLayout());
 		
+		//Initialize the savings and checking accounts.
 		checkingAccount = new Account(CHECKING_DEFAULT_AMOUNT);
 		savingsAccount = new Account(SAVINGS_DEFAULT_AMOUNT);
 		
+		//Create and label the various buttons and prepare to listen to them.
 		JButton withdraw = new JButton("Withdraw");
 		withdraw.setActionCommand("withdraw");
 		
@@ -55,6 +65,7 @@ public class BankGUI extends JPanel implements ActionListener, PropertyChangeLis
 		JRadioButtonMenuItem savings = new JRadioButtonMenuItem("Savings");
 		savings.setActionCommand("savings");
 		
+		//Group the radio buttons together and set the default account to be checking.
 		ButtonGroup accounts = new ButtonGroup();
 		accounts.add(savings);
 		accounts.add(checking);
@@ -62,11 +73,13 @@ public class BankGUI extends JPanel implements ActionListener, PropertyChangeLis
 		account = checkingAccount;
 		offAccount = savingsAccount;
 		
+		//Create a modified JFormattedTextField with a currency format, set it to 0 and start listening to it.
 		amountField = new AmountField(currency);
 		amountField.setColumns(20);
 		amountField.setValue(new Double(0.00));
 		amountField.addPropertyChangeListener("value", this);
 		
+		//Start listening to the buttons.
 		withdraw.addActionListener(this);
 		deposit.addActionListener(this);
 		transfer.addActionListener(this);
@@ -74,26 +87,31 @@ public class BankGUI extends JPanel implements ActionListener, PropertyChangeLis
 		savings.addActionListener(this);
 		checking.addActionListener(this);
 		
+		//Create tooltips for the buttons describing their function.
 		withdraw.setToolTipText("Withdraw the typed amount from the selected account. (Must be in increments of $20)");
 		deposit.setToolTipText("Deposit the typed amount to the selected account.");
 		transfer.setToolTipText("Transfer the typed amount to the selected account from the other account.");
 		balance.setToolTipText("Displays current balance of selected account.");
 		
+		//Add the transactionButtons panel and put the relevant buttons in that panel.
 		add(transactionButtons);
 		transactionButtons.add(withdraw);
 		transactionButtons.add(deposit);
 		transactionButtons.add(transfer);
 		transactionButtons.add(balance);
 		
+		//Add the accountButtons panel and put the relevant buttons in that panel.
 		add(accountButtons);
 		accountButtons.add(checking);
 		accountButtons.add(savings);
 		
+		//Add the textPanel panel and put the text field in that panel.
 		add(textPanel);
 		textPanel.add(amountField);
 	}
 	
 	public static void main(String args[]) {
+		//Create the top-level frame with a BankGUI in it.
 		JFrame frame = new JFrame("ATM");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -106,48 +124,68 @@ public class BankGUI extends JPanel implements ActionListener, PropertyChangeLis
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		if("withdraw".equals(e.getActionCommand())) {
-			if(((amount % 20) == 0) && (amount != 0)) {
+		//When the "Withdraw" button is pressed, check to see if the amount in the text field is divisible by 20 and ensure that it is not zero.
+		if ("withdraw".equals(e.getActionCommand())) {
+			if (((amount % 20) == 0) && (amount != 0)) {
+				
+				//Attempt to withdraw the amount from the selected account. If successful, inform the user. Warn and inform user of service charges.
 				try {
 					account.withdraw(amount);
 					JOptionPane.showMessageDialog(PopupFrame, "Withdrawl of " + currency.format(amount) + " successful." + Account.serviceChargeApplied());
 				}
-				catch(InsufficientFunds ex) {
+				
+				//If unsuccessful, inform the user. Inform user of service charges.
+				catch (InsufficientFunds ex) {
 					JOptionPane.showMessageDialog(PopupFrame, "Insufficient funds in the account." + Account.serviceChargeApplied());
 				}
 			}
+			
+			//If amount is not divisible by 20 or is zero, inform user of the proper amounts.
 			else {
 				JOptionPane.showMessageDialog(PopupFrame, "Withdrawls must be in increments of $20.");
 			}
 		}
-		else if("deposit".equals(e.getActionCommand())) {
+		
+		//When the "Deposit" button is pressed, deposit the amount in the text field to the selected account.
+		else if ("deposit".equals(e.getActionCommand())) {
 			account.deposit(amount);
 		}
-		else if("transfer".equals(e.getActionCommand())) {
+		
+		//When the "Transfer" button is pressed, transfer the amount in the text field from the unselected account to the selected account.
+		else if ("transfer".equals(e.getActionCommand())) {
 			try {
 				offAccount.transfer(amount, account);
 			}
-			catch(InsufficientFunds ex) {
+			
+			//If there isn't enough money in the unselected account, inform the user.
+			catch (InsufficientFunds ex) {
 				JOptionPane.showMessageDialog(PopupFrame, "Insufficient funds in the account.");
 			}
 		}
-		else if("balance".equals(e.getActionCommand())) {
+		
+		//When the "Balance" button is pressed, inform the user of the amount contained in the selected account.
+		else if ("balance".equals(e.getActionCommand())) {
 			JOptionPane.showMessageDialog(PopupFrame, "The current balance is " + currency.format(account.balance()) + ".");
 		}
-		else if("checking".equals(e.getActionCommand())) {
+		
+		//When the "Checking" button is pressed, switch the active account to checking.
+		else if ("checking".equals(e.getActionCommand())) {
 			account = checkingAccount;
 			offAccount = savingsAccount;
 		}
-		else if("savings".equals(e.getActionCommand())) {
+		
+		//When the "Savings" button is pressed, switch the active account to savings.
+		else if ("savings".equals(e.getActionCommand())) {
 			account = savingsAccount;
 			offAccount = checkingAccount;
 		}
 	}
 
+	//When the information in the text field changes, if it is a valid input, set amount to the new value.
 	public void propertyChange(PropertyChangeEvent e) {
 		Object source = e.getSource();
 		
-		if(source == amountField) {
+		if (source == amountField) {
 			amount = ((Number)amountField.getValue()).doubleValue();
 		}
 	}
