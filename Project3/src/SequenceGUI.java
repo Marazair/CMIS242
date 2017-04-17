448aaf75a2ae2ba058242cae489c2e0bd40cd197
Marazair <nick1022m@gmail.com>
2017-04-17 13:01:34 -0400

    Start SequenceGUI
    Created some basic code for SequenceGUI.

--- Project3/src/SequenceGUI.java
+++ Project3/src/SequenceGUI.java
@@ -1,19 +1,41 @@
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class SequenceGUI extends JPanel implements ActionListener {
 	public SequenceGUI() {
+		super.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+		this.setBorder(padding);
+		
+		JRadioButtonMenuItem iterative = new JRadioButtonMenuItem("Iterative");
+		iterative.setActionCommand("iterative");
+		
+		JRadioButtonMenuItem recursive = new JRadioButtonMenuItem("Recursive");
+		recursive.setActionCommand("recursive");
+		
 		
 	}
 	
 	
 	public static void main(String[] args) {
+		JFrame frame = new JFrame("Sequence");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-
+		SequenceGUI sequence = new SequenceGUI();
+		sequence.setOpaque(true);
+		frame.setContentPane(sequence);
+		
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+	public class FileEventHandler extends WindowAdapter{
 		
 	}
 
