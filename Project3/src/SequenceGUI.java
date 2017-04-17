48bc154f9391d5921dd350bee39c6444dc1d7994
Marazair <nick1022m@gmail.com>
2017-04-17 12:48:47 -0400

    Created SequenceGUI.java

---
+++ Project3/src/SequenceGUI.java
@@ -0,0 +1,20 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SequenceGUI extends JPanel implements ActionListener {
+	public SequenceGUI() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
