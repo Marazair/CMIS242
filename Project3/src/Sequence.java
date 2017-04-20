c6ee6738a5156e9ab31180be575de2940cba545b
Marazair <nick1022m@gmail.com>
2017-04-19 20:00:47 -0400

    Sequence Basic Structure
    Created the basic structure for Sequence.

---
+++ Project3/src/Sequence.java
@@ -0,0 +1,24 @@
+
+public final class Sequence {
+	private static int efficiency = 0;
+	
+	private Sequence() {
+		
+	}
+	
+	public static int computeIterative(int n) {
+		
+	}
+	
+	public static int computeRecursive(int n) {
+		
+	}
+	
+	private static int recursive (int n) {
+		
+	}
+	
+	public static int getEfficiency() {
+		
+	}
+}
