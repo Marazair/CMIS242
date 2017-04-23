e22ab8c6c391f4885c9604183ca9e649779f5faa
Marazair <nick1022m@gmail.com>
2017-04-23 09:17:41 -0400

    Comments
    Added all the comments.

--- Project3/src/Sequence.java
+++ Project3/src/Sequence.java
@@ -4,22 +4,30 @@
  * Date: 4/17/17
  * Purpose: Calculate the numbers in the sequence using both iterative and recursive methods.
  */
+
+//Declare class as final so that nothing can extend this class.
 public final class Sequence {
 	
+	//Create static variable for keeping track of the most recent function call's efficiency.
 	private static int efficiency = 0;
 	
-	
+	//Declare a private constructor so that nothing can make an instance of this class.
 	private Sequence() {
 		
 	}
 	
+	//Method for computing the nth term using iteration.
 	public static int computeIterative(int n) {
+		//Reset the efficiency counter.
 		efficiency = 0;
+		
+		
 		int currentTerm = 0;
 		int previousTerm = 0;
 		int temp = 0;
 		
 		for (int x = 0; x <= n; x++) {
+			
 			if (x == 0) {
 				currentTerm = 0;
 			}
@@ -38,11 +46,14 @@ public final class Sequence {
 		return currentTerm;
 	}
 	
+	//Method that resets the efficiency counter then calls the recursive calculation method.
 	public static int computeRecursive(int n) {
 		efficiency = 0;
 		return recursive(n);
 	}
 	
+	//Method that calculates the nth term using recursion. 
+	//Set to private to ensure efficiency is reset before the calculations are made.
 	private static int recursive (int n) {
 		if (n == 0) {
 			efficiency++;
@@ -56,6 +67,7 @@ public final class Sequence {
 		return 2 * recursive(n-1) + recursive(n-2);
 	}
 	
+	//Returns the efficiency of the last call to either the recursive or iterative methods.
 	public static int getEfficiency() {
 		return efficiency;
 	}
--- Project3/src/SequenceGUI.java
+++ Project3/src/SequenceGUI.java
@@ -20,34 +20,41 @@ public class SequenceGUI extends JPanel implements ActionListener {
 	private JRadioButtonMenuItem recursive = new JRadioButtonMenuItem("Recursive");
 	private JRadioButtonMenuItem iterative = new JRadioButtonMenuItem("Iterative");
 	
+	//Create a variable for holding the value of n.
 	private int n;
 	
 	public SequenceGUI() {
+		//Create the panel and make padding around the edges.
 		super.setLayout(new GridLayout(5,2,10,10));
 		Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
 		this.setBorder(padding);
 		
+		//Create a panel for organizing the radio buttons and set iterative to be the default calculation method.
 		JPanel radioPanel = new JPanel(new GridLayout(2,1,0,0));
 		iterative.setSelected(true);
 		
+		//Group the radio buttons so that they work properly.
 		ButtonGroup methods = new ButtonGroup();
 		methods.add(iterative);
 		methods.add(recursive);
 		
+		//Create the Compute button.
 		JButton compute = new JButton("Compute");
 		compute.setActionCommand("compute");
+		compute.addActionListener(this);
 		
+		//Create the labels.
 		JLabel nLabel = new JLabel("Enter n:");
 		JLabel resultLabel = new JLabel("Result:");
 		JLabel efficiency = new JLabel("Efficiency:");
 		
+		//Make sure the result and efficiency fields are blank to begin with, then make them uneditable.
 		resultField.setText("");
 		resultField.setEditable(false);
 		efficiencyField.setText("");
 		efficiencyField.setEditable(false);
 		
-		compute.addActionListener(this);
-		
+		//Add all the components to the panel, including blank components to fill up grid slots.
 		add(new JLabel(""));
 		
 		radioPanel.add(iterative);
@@ -66,14 +73,17 @@ public class SequenceGUI extends JPanel implements ActionListener {
 	
 	
 	public static void main(String[] args) {
+		//Create the frame, content panel and window close event handler.
 		JFrame frame = new JFrame("Sequence");
-		
 		SequenceGUI sequence = new SequenceGUI();
 		SequenceFileEventHandler fileWriter = new SequenceFileEventHandler();
+		
+		//Attach the content pane and window close event handler to the frame.
 		sequence.setOpaque(true);
 		frame.setContentPane(sequence);
 		frame.addWindowListener(fileWriter);
 		
+		//Display the frame.
 		frame.pack();
 		frame.setVisible(true);
 			
@@ -81,20 +91,28 @@ public class SequenceGUI extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		//Give logic for when the compute button is pressed.
 		if ("compute".equals(e.getActionCommand())) {
 			
 			try {
+				//Changed the contents of n into an integer.
 				n = Integer.parseInt(nField.getText());
 				
+				//If iterative is selected, pass n to the method which computes the result in the iterative way, 
+				//then set that result to the result field.
 				if (iterative.isSelected()) {
 					resultField.setText(Integer.toString(Sequence.computeIterative(n)));
 				}
+				//If recursive is selected, pass n to the method which computes the result in the recursive way,
+				//then set that result to the result field.
 				else if (recursive.isSelected()) {
 					resultField.setText(Integer.toString(Sequence.computeRecursive(n)));
 				}
 				
+				//Set the efficiency field to the efficiency of the most recently run calculation method.
 				efficiencyField.setText(Integer.toString(Sequence.getEfficiency()));
 			}
+			//If the text field contains something that cannot be translated into an integer, inform the user.
 			catch (NumberFormatException ex) {
 				JFrame popupFrame = new JFrame("Popup");
 				JOptionPane.showMessageDialog(popupFrame, "Please put an integer into the field for n.");
@@ -108,7 +126,11 @@ public class SequenceGUI extends JPanel implements ActionListener {
 		}
 		
 		@Override
+		//This method is called when the window the handler is attached to is closing.
 		public void windowClosing(WindowEvent e) {
+			
+			//Attempt to write a file to info.txt that contains the values of n, iterative efficiency and recursive efficiency
+			//for values of n up to and including 10.
 			try {
 				PrintWriter writer = new PrintWriter("info.txt", "UTF-8");
 				for(int x = 0; x <= 10; x++){
@@ -120,9 +142,13 @@ public class SequenceGUI extends JPanel implements ActionListener {
 					writer.println();
 				}
 				writer.close();
+				
+				//Inform the user that the file has been written.
 				JFrame popupFrame = new JFrame("Popup");
 				JOptionPane.showMessageDialog(popupFrame, "File has been written.");
 			}
+			
+			//If there is a problem writing to the file, inform the user.
 			catch (IOException ex) {
 				JFrame popupFrame = new JFrame("Popup");
 				JOptionPane.showMessageDialog(popupFrame, "Problem with writing to file.");
