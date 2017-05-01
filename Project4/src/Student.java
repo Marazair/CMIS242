7c2aa787d5d97aaad98ad568feff064108fb47bf
Marazair <nick1022m@gmail.com>
2017-05-01 11:11:16 -0400

    Student Class Complete

--- Project4/src/Student.java
+++ Project4/src/Student.java
@@ -1,4 +1,21 @@
 
 public class Student {
-
+	private String name;
+	private String major;
+	private int totalCredits;
+	private int qualityPoints;
+	
+	public Student(String name, String major) {
+		this.name = name;
+		this.major = major;
+	}
+	
+	public void courseCompleted(int grade, int credits) {
+		totalCredits += credits;
+		qualityPoints += (grade * credits);
+	}
+	
+	public String toString() {
+		return "Name: " + name + ", Major: " + major + ", GPA: " + (totalCredits/qualityPoints);
+	}
 }
