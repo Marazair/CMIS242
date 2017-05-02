487a5e5776930a7808dbdf703fbc154ed2f28d02
Marazair <nick1022m@gmail.com>
2017-05-02 12:58:11 -0400

    Created GUI Elements
    Created all GUI elements for the program, no logic behind it yet.

--- Project4/src/StudentDatabase.java
+++ Project4/src/StudentDatabase.java
@@ -1,4 +1,57 @@
+import java.awt.*;
+import java.awt.event.*;
 
-public class StudentDatabase {
+import javax.swing.*;
+import javax.swing.border.*;
 
+public class StudentDatabase extends JPanel implements ActionListener{
+	private JTextField idField = new JTextField(10);
+	private JTextField nameField = new JTextField(10);
+	private JTextField majorField = new JTextField(10);
+	private JComboBox<String> optionDropdown;
+	
+	public StudentDatabase() {
+		super.setLayout(new GridLayout(5,2,10,10));
+		Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+		this.setBorder(padding);
+		
+		JLabel idLabel = new JLabel("ID:");
+		JLabel nameLabel = new JLabel("Name:");
+		JLabel majorLabel = new JLabel("Major:");
+		JLabel selectionLabel = new JLabel("Choose Selection:");
+		
+		String[] options = {"Insert","Delete","Find","Update"};
+		optionDropdown = new JComboBox<String>(options);
+		
+		JButton processButton = new JButton("Process Request");
+		processButton.addActionListener(this);
+		
+		add(idLabel);
+		add(idField);
+		add(nameLabel);
+		add(nameField);
+		add(majorLabel);
+		add(majorField);
+		add(selectionLabel);
+		add(optionDropdown);
+		add(processButton);
+	}
+	
+	public static void main(String args[]) {
+		JFrame frame = new JFrame("Student Database");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		StudentDatabase school = new StudentDatabase();
+		school.setOpaque(true);
+		frame.setContentPane(school);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
 }
