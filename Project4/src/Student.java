4e177d078cd44321e06fde2018e3e04783d49832
Marazair <nick1022m@gmail.com>
2017-05-05 18:01:26 -0400

    Completed Program sans Comments

--- Project4/src/Student.java
+++ Project4/src/Student.java
@@ -1,3 +1,4 @@
+import java.text.*;
 
 public class Student {
 	private String name;
@@ -6,16 +7,39 @@ public class Student {
 	private int qualityPoints;
 	
 	public Student(String name, String major) {
+		totalCredits = 0;
+		qualityPoints = 0;
 		this.name = name;
 		this.major = major;
 	}
 	
-	public void courseCompleted(int grade, int credits) {
+	public void courseCompleted(char grade, int credits) {
 		totalCredits += credits;
-		qualityPoints += (grade * credits);
+		
+		if (grade == 'A') {
+			qualityPoints += (4*credits);
+		}
+		else if(grade == 'B') {
+			qualityPoints += (3*credits);
+		}
+		else if(grade == 'C') {
+			qualityPoints += (2*credits);
+		}
+		else if(grade == 'D') {
+			qualityPoints += (1*credits);
+		}
+		else if(grade == 'F') {
+			qualityPoints += (0*credits);
+		}
 	}
 	
 	public String toString() {
-		return "Name: " + name + ", Major: " + major + ", GPA: " + (totalCredits/qualityPoints);
+		DecimalFormat numberFormat = new DecimalFormat("#.0");
+		if(totalCredits != 0) {
+			return "Name: " + name + ", Major: " + major + ", GPA: " + (numberFormat.format((double)qualityPoints/totalCredits));
+		}
+		else {
+			return "Name: " + name + ", Major: " + major + ", GPA: 4.0";
+		}
 	}
 }
--- Project4/src/StudentDatabase.java
+++ Project4/src/StudentDatabase.java
@@ -1,14 +1,16 @@
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
 import javax.swing.border.*;
+import java.util.*;
 
 public class StudentDatabase extends JPanel implements ActionListener{
 	private JTextField idField = new JTextField(10);
 	private JTextField nameField = new JTextField(10);
 	private JTextField majorField = new JTextField(10);
 	private JComboBox<String> optionDropdown;
+	HashMap<Integer, Student> database = new HashMap<Integer, Student>();
+	
 	
 	public StudentDatabase() {
 		super.setLayout(new GridLayout(5,2,10,10));
@@ -20,8 +22,13 @@ public class StudentDatabase extends JPanel implements ActionListener{
 		JLabel majorLabel = new JLabel("Major:");
 		JLabel selectionLabel = new JLabel("Choose Selection:");
 		
+		idField.setText("");
+		nameField.setText("");
+		majorField.setText("");
+		
 		String[] options = {"Insert","Delete","Find","Update"};
 		optionDropdown = new JComboBox<String>(options);
+		optionDropdown.setSelectedIndex(0);
 		
 		JButton processButton = new JButton("Process Request");
 		processButton.addActionListener(this);
@@ -51,7 +58,72 @@ public class StudentDatabase extends JPanel implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		JFrame popupFrame = new JFrame();
 		
-		
+		try {
+			int key = Integer.parseInt(idField.getText());
+			String name = nameField.getText();
+			String major = majorField.getText();
+			String currentOption = optionDropdown.getSelectedItem().toString();
+			
+			if((!name.equals("") && !major.equals("")) || !currentOption.equals("Insert")) {
+				if (currentOption.equals("Insert")) {
+					if (database.containsKey(key)) {
+						JOptionPane.showMessageDialog(popupFrame, "Student ID already in use. Please select a different ID.");
+					}
+					else {
+						database.put(key, new Student(name, major));
+						JOptionPane.showMessageDialog(popupFrame, "Insertion of student " + key + ": " + database.get(key).toString() + " successful.");
+					}
+				}
+				else if (currentOption.equals("Delete")) {
+					if (database.containsKey(key)) {
+						JOptionPane.showMessageDialog(popupFrame, "Student " + key + ": " + database.get(key).toString() + " will be removed.");
+						database.remove(key);
+					}
+					else {
+						JOptionPane.showMessageDialog(popupFrame, "Database does not contain the specified student ID.");
+					}
+				}
+				else if (currentOption.equals("Find")) {
+					if (database.containsKey(key)) {
+						JOptionPane.showMessageDialog(popupFrame, "Student " + key + ": " + database.get(key).toString());
+					}
+					else {
+						JOptionPane.showMessageDialog(popupFrame, "Database does not contain the specified student ID.");
+					}
+				}
+				else if (currentOption.equals("Update")) {
+					if (database.containsKey(key)) {
+						Object[] gradeChoices = {'A','B','C','D','F'};
+						char grade = (char)JOptionPane.showInputDialog(null, "Choose grade:", "Grade", 
+													JOptionPane.INFORMATION_MESSAGE, null, gradeChoices, gradeChoices[0]);
+						
+						Object[] creditsChoices = {3,6};
+						int credits = (int)JOptionPane.showInputDialog(null, "Choose credits:", "Credits", 
+													JOptionPane.INFORMATION_MESSAGE, null, creditsChoices, creditsChoices[0]);
+						
+						database.get(key).courseCompleted(grade, credits);
+						
+						JOptionPane.showMessageDialog(popupFrame, "Student " + key + ": " + database.get(key).toString() + ". Update complete.");
+					}
+					else {
+						JOptionPane.showMessageDialog(popupFrame, "Database does not contain the specified student ID.");
+					}
+				}
+			}
+			else if(name.equals("") && major.equals("")) {
+				JOptionPane.showMessageDialog(popupFrame, "Please put a name and major in the appropriate fields.");
+			}
+			else if(name.equals("")) {
+				JOptionPane.showMessageDialog(popupFrame, "Please put a name in the appropriate field.");
+			}
+			else if(major.equals("")) {
+				JOptionPane.showMessageDialog(popupFrame, "Please put a major in the appropriate field.");
+			}
+		}
+		catch (NumberFormatException ex) {
+			JOptionPane.showMessageDialog(popupFrame, "Please put a number into the ID field.");
+		}
 	}
 }
